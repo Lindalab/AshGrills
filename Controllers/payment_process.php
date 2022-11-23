a95b4a895bc22bf92187b65db7a53acf5216387f
678Elaine <94685092+678Elaine@users.noreply.github.com>
2022-11-23 08:40:34 +0000

    Update payment_process.php

--- Controllers/payment_process.php
+++ Controllers/payment_process.php
@@ -2,10 +2,10 @@
     require_once("../Classes/payment_class.php");
 
 
-    function payment_process($amount, $customer_id, $currency, $payment_Date, $booked_slot_id){
+    function payment_controller($amount, $customer_id, $currency, $payment_Date, $booked_slot_id){
         $pay = new payment;
 
-        return $pay->payment_process($amount, $customer_id, $currency, $payment_Date, $booked_slot_id);
+        return $pay->payment_controller($amount, $customer_id, $currency, $payment_Date, $booked_slot_id);
     }
 
 
