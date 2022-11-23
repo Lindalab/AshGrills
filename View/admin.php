0113e593354277a7d673ab1c3f5f4b5ba61e873d
Lindalab <linda.arthur@ashesi.edu.gh>
2022-11-23 03:20:03 +0000

    mice

--- View/admin.php
+++ View/admin.php
@@ -78,9 +78,9 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="index.php" class="nav-item nav-link">Home</a>
+                <!-- <a href="index.php" class="nav-item nav-link">Home</a>
                 <a href="about.html" class="nav-item nav-link">About</a>
-                <a href="services.php" class="nav-item nav-link" >Appointment</a> -->
+                <a href="services.php" class="nav-item nav-link" >Appointment</a>  -->
 
             </div>
             <!-- <a href="contact.php" class="nav-item nav-link active">Contact</a> -->
@@ -97,7 +97,7 @@
             Add a service
         </a>
         <span>&nbsp;</span>
-        <a href="login.php" class="btn btn-primary py-2 px-4 d-none d-lg-block">Booking</a>
+        <a href="login.php" class="btn btn-primary py-2 px-4 d-none d-lg-block">Logout</a>
         </div>
     </nav>
     <!-- Navbar End -->
@@ -106,12 +106,12 @@
     <!-- Page Header Start -->
     <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
-            <h1 class="display-1 text-white animated slideInDown">Contact Us</h1>
+            <h1 class="display-1 text-white animated slideInDown">Welcome</h1>
             <nav aria-label="breadcrumb animated slideInDown">
                 <ol class="breadcrumb text-uppercase mb-0">
-                    <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
+                    <!-- <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
                     <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
-                    <li class="breadcrumb-item text-primary active" aria-current="page">Contact Us</li>
+                    <li class="breadcrumb-item text-primary active" aria-current="page">Contact Us</li> -->
                 </ol>
             </nav>
         </div>
@@ -120,81 +120,7 @@
 
 
     <!-- Contact Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-                <h4 class="section-title">Contact Us</h4>
-                <h1 class="display-5 mb-4">If You Have Any Query, Please Feel Free Contact Us</h1>
-            </div>
-            <div class="row g-5">
-                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="d-flex flex-column justify-content-between h-100">
-                        <div class="bg-light d-flex align-items-center w-100 p-4 mb-4">
-                            <div class="d-flex flex-shrink-0 align-items-center justify-content-center bg-dark" style="width: 55px; height: 55px;">
-                                <i class="fa fa-map-marker-alt text-primary"></i>
-                            </div>
-                            <div class="ms-4">
-                                <p class="mb-2">Address</p>
-                                <h3 class="mb-0">1 University Avenue, Berekuso, Ghana.</h3>
-                            </div>
-                        </div>
-                        <div class="bg-light d-flex align-items-center w-100 p-4 mb-4">
-                            <div class="d-flex flex-shrink-0 align-items-center justify-content-center bg-dark" style="width: 55px; height: 55px;">
-                                <i class="fa fa-phone-alt text-primary"></i>
-                            </div>
-                            <div class="ms-4">
-                                <p class="mb-2">Call Us Now</p>
-                                <h3 class="mb-0">+233 20 582 5252</h3>
-                            </div>
-                        </div>
-                        <div class="bg-light d-flex align-items-center w-100 p-4">
-                            <div class="d-flex flex-shrink-0 align-items-center justify-content-center bg-dark" style="width: 55px; height: 55px;">
-                                <i class="fa fa-envelope-open text-primary"></i>
-                            </div>
-                            <div class="ms-4">
-                                <p class="mb-2">Mail Us Now</p>
-                                <h3 class="mb-0">ashgrill@ashesi.edu.gh</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <p class="mb-4">The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
-                    <form>
-                        <div class="row g-3">
-                            <div class="col-md-6">
-                                <div class="form-floating">
-                                    <input type="text" class="form-control" id="name" placeholder="Your Name">
-                                    <label for="name">Your Name</label>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-floating">
-                                    <input type="email" class="form-control" id="email" placeholder="Your Email">
-                                    <label for="email">Your Email</label>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-floating">
-                                    <input type="text" class="form-control" id="subject" placeholder="Subject">
-                                    <label for="subject">Subject</label>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-floating">
-                                    <textarea class="form-control" placeholder="Leave a message here" id="message" style="height: 100px"></textarea>
-                                    <label for="message">Message</label>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <button class="btn btn-primary w-100 py-3" type="submit">Send Message</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+    
 
 
     <!-- Button trigger modal -->
@@ -207,7 +133,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Service</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -218,7 +144,7 @@
                     <form action="../Login/register_process.php" method="GET">
                         <div class="form-outline mb-4">
                             <label class="form-label" for="formName">Serivce Name</label>
-                            <input type="text" id="formEmail" name="service_name" class="form-control" placeholder="full name" required />
+                            <input type="text" id="formEmail" name="service_name" class="form-control" placeholder="service name" required />
                         </div>
 
 
@@ -229,24 +155,24 @@
 
                         <div class="form-outline mb-4">
                             <label class="form-label" for="formPassword">Price</label>
-                            <input type="password" id="formPassword" name="price" class="form-control" required />
+                            <input type="number" id="formPassword" name="price" class="form-control" required />
                         </div>
 
                         <div class="form-outline mb-4">
                             <label class="form-label" for="formCountry">Service Image</label>
-                            <input type="file" id="formCountry" name="service_image" class="form-control" placeholder="Country" />
+                            <input type="file" id="formCountry" name="service_image" class="form-control" placeholder="Service Image" />
                         </div>
 
 
                         <div class="text-center pt-1 mb-5 pb-1">
-                            <input type="submit" class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" class="form-control" name="submit" value="Register" />
+                            <input type="submit" class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" class="form-control" name="submit" value="Add Service" />
 
                         </div>
                     </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Create Service</button>
+                   
                 </div>
             </div>
         </div>
@@ -258,7 +184,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Appointments</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -273,26 +199,26 @@
 
                         <div class="form-outline mb-4">
                             <label class="form-label" for="formEmail">Appointment day</label>
-                            <input type="email" id="formEmail" name="appointment_day" class="form-control" placeholder="Email address" required />
+                            <input type="text" id="formEmail" name="appointment_day" class="form-control" placeholder="Email address" required />
                         </div>
 
                         <div class="form-outline mb-4">
                             <label class="form-label" for="formPassword">Appointment time</label>
-                            <input type="password" id="formPassword" name="price" class="form-control" required />
+                            <input type="time" id="formPassword" name="price" class="form-control" required />
                         </div>
 
 
 
 
                         <div class="text-center pt-1 mb-5 pb-1">
-                            <input type="submit" class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" class="form-control" name="submit" value="Register" />
+                            <input type="submit" class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" class="form-control" name="submit" value="Add Appointments" />
 
                         </div>
                     </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Create Service</button>
+                   
                 </div>
             </div>
         </div>
@@ -303,7 +229,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Category</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -313,21 +239,21 @@
                                         <div class="form-outline mb-4">
                                             <label class="form-label" for="formName">Category Name</label>
                                             <input type="text" id="formEmail" name="service_name" class="form-control"
-                                                placeholder="full name" required />
+                                                placeholder="Category Name" required />
                                         </div>
 
 
                                         <div class="text-center pt-1 mb-5 pb-1">
                                             <input type="submit"
                                                 class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
-                                                class="form-control" name="submit" value="Register" />
+                                                class="form-control" name="submit" value="Add Categories" />
 
                                         </div>
                                         </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Create Service</button>
+                    
                 </div>
             </div>
         </div>
@@ -336,9 +262,9 @@
 
 
     <!-- Google Map Start -->
-    <div class="container-xxl pt-5 px-0 wow fadeIn" data-wow-delay="0.1s">
+    <!-- <div class="container-xxl pt-5 px-0 wow fadeIn" data-wow-delay="0.1s">
         <iframe class="w-100 mb-n2" style="height: 450px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-    </div>
+    </div> -->
     <!-- Google Map End -->
 
 
